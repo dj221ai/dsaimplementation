6fe109656724f2975b9ece0ebf6ba97b4ca66593
Ved Jha <vedprakash@fanclash.in>
2022-05-29 15:47:54 +0530

    insertion n deletion in doubly linked list

--- linkedlist/doublylinkedlist.py
+++ linkedlist/doublylinkedlist.py
@@ -74,6 +74,61 @@ class LinkedList:
                 currnode = currnode.next
                 counter += 1
 
+    #deletion starts from here
+
+    #delete at end
+    def deleteAtEnd(self):
+        if self.isEmpty() is not True:
+            return
+        # checking if list has only one node
+        elif self.head.next is None:
+            print("head next is none")
+            self.deleteAtHead()
+            return
+        else:
+            currnode = self.head
+            while True:
+                if currnode.next is None:
+                    currnode.previous.next = None
+                    # del currnode
+                    break
+                currnode = currnode.next
+
+    #delete at head
+    def deleteAtHead(self):
+        if self.isEmpty() is True and self.head.next is not None:
+            self.head = self.head.next
+            self.head.previous.next = None
+            self.head.previous = None
+            return
+        elif self.isEmpty() is True and self.head.next is None:
+            del self.head
+            return
+        else:
+            print("List is Empty can't delete Head Node")
+
+
+    #delete in between
+    def deleteInBetween(self, posval):
+        if posval < 0 or posval > self.checkLength():
+            print("Invalid position provided can't delete")
+            return
+        elif posval == 0:
+            self.deleteAtHead()
+            return
+        else:
+            curnode = self.head
+            counter = 0
+            while True:
+                if counter == posval:
+                    curnode.previous.next = curnode.next
+                    curnode.next.previous = curnode.previous
+                    break
+                curnode = curnode.next
+                counter += 1
+
+
+
                 
 
     def printDLL(self):
@@ -103,11 +158,20 @@ node2 = Node('node 2')
 node3 = Node('node 3')
 node4 = Node('node 4')
 node5 = Node('node 5')
+node6 = Node('node 6')
+node7 = Node('node 7')
+node8 = Node('node 8')
 dll = LinkedList()
 dll.insertAtHead(node1)
-# dll.insertAtEnd(node2)
 dll.insertAtEnd(node3)
 dll.insertInBetween(node2, 1)
-# dll.insertAtHead(node4)
-# dll.insertAtHead(node5)
+dll.insertAtHead(node4)
+dll.insertAtEnd(node5)
+dll.insertAtEnd(node6)
+print("before deleting >>>>>>>>>>>>>>>>")
+dll.printDLL()
+dll.deleteAtEnd()
+dll.deleteAtHead()
+dll.deleteInBetween(0)
+print("After deleting >>>>>>>>>>>>>>>>")
 dll.printDLL()
